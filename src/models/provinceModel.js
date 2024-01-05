957a3f1196abfbad7331b138d7cf39df0abde520
Ilham Karyanto <ilham.karyanto@rupiahtoken.com>
2024-01-06 01:52:04 +0700

    fix query

--- src/models/provinceModel.js
+++ src/models/provinceModel.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 const userModel = {
     async insertProvinvce(provinceName, provinceCode) {
-        const queryText = 'INSERT INTO city (province_name, province_code) VALUES ($1, $2) RETURNING id';
+        const queryText = 'INSERT INTO province (province_name, province_code) VALUES ($1, $2) RETURNING id';
         const values = [provinceName, provinceCode];
         const { rows } = await db.query(queryText, values);
         return rows[0].id;
